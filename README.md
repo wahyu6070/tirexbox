c6d48da9bbd88a936faf6f0853d56023a9fbccf7
wahyu6070 <gwtsendangrejo6070@gmail.com>
2021-06-02 11:57:01 +0700

    imp

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 #tirexbox
+- tirexbox is an android rom customization that aims to optimize your rom
--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,10 @@
 LIST_RM="
-
+/data/adb/service.d/tirexbox-post-fs
 
 "
+for W in $LIST_RM; do
+rm -rf $W
+done
 #set default animation scale android
 settings put global transition_animation_scale 1.0
 settings put global window_animation_scale 1.0
